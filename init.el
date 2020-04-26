d33525e1336df9ded514c17f5ee24c93d90e7602
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2020-04-26 22:16:37 +0300

    Add custom.el settings

--- init.el
+++ init.el
@@ -1,21 +1,6 @@
 (defvar config-org (expand-file-name "README.org" user-emacs-directory))
 (defvar config-el (expand-file-name "README.el" user-emacs-directory))
- 
+
 (if (file-exists-p config-el)
     (load-file config-el)
   (org-babel-load-file config-org))(setq inhibit-startup-message t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(sanityinc-tomorrow-blue))
- '(custom-safe-themes
-   '("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default))
- '(package-selected-packages '(shackle doom-modeline use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
